38be141e58d216c5d52e51730b26b0e4f706925e
Ej_Dev <you@example.com>
2024-03-06 13:56:18 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const navlinklist = document.querySelector(".nav-ul");
+const navlink = document.querySelector("#navlinks");
+const openBtn = document.querySelector("#openBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const imagelist = document.querySelectorAll(".img")
+const listItems = document.querySelectorAll('#ul li');
+
+
+  
+listItems.forEach(item => {
+    item.addEventListener('click',() =>{
+        if(window.innerWidth < 2000){
+        closeBtnFunction();
+        }
+    });
+});
+
+openBtn.addEventListener("click", ()=>{
+    navlinklist.style.left="0";
+    openBtn.style.display ="none";
+    closeBtn.style.display ="flex";
+   
+});
+
+closeBtn.addEventListener("click", ()=>{
+    navlinklist.style.left="-400px";
+    closeBtn.style.display ="none";
+    openBtn.style.display ="flex";
+});
+
+function closeBtnFunction() {
+   navlinklist.style.left= "-400px" ; 
+    closeBtn.style.display = "none";
+    openBtn.style.display = "flex";
+}
+
+
+
+
+
+window.addEventListener("load", () => {
+
+
+    const fade1 = document.querySelector(".fade1");
+        fade1.style.left = "100%";   
+});
+
+
+
+const nav = document.querySelector("#nav");
+var lastScrollPosition = 0;
+
+window.addEventListener( 'scroll', function(){
+    var currentScrollPosition = window.scrollY;
+    
+    if (currentScrollPosition < lastScrollPosition) {
+        nav.style.position = 'fixed';
+        
+    } else{
+        nav.style.position = 'relative'; 
+    }
+
+    lastScrollPosition = currentScrollPosition;
+});
+
